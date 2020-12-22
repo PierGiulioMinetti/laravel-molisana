9f32f884cc0aa6849f1f154a3e75b17540d9bdd2
Pier Giulio Minetti <pg.minetti@gmail.com>
2020-12-22 23:24:38 +0100

    Fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,40 @@
 
 @section('content')
 
-<section class="container">
-    <div class="cards">
-        @foreach ($cards as $card)
+   <section class="type-section container">
+       <h2>LE LUNGHE</h2>
+       @foreach ($lunghe as $card)
             <div class="card">
-                <img src=" {{$card['src']}} " alt="">
-                <h3>
-                    {{ $card['titolo']}}
-                </h3>
-                <a href="">
-
-                </a>
+                <h3> {{ $card['titolo'] }} </h3>
+                <img src=" {{ $card['src'] }}" >
+                <a href=""> Vedi prodotto</a>
             </div>
-        @endforeach
-    </div>
+       @endforeach
+   </section>
 
-
+   <section class="type-section container">
+    <h2>LE CORTE</h2>
+    @foreach ($corte as $card)
+         <div class="card">
+             <img src=" {{ $card['src'] }}" >
+             <h3> {{ $card['titolo'] }} </h3>
+             <a href=""> Vedi prodotto</a>
+         </div>
+    @endforeach
 </section>
+
+<section class="type-section container">
+    <h2>LE CORTE</h2>
+    @foreach ($cortissime as $card)
+         <div class="card">
+             <img src=" {{ $card['src'] }}" >
+             <h3> {{ $card['titolo'] }} </h3>
+             <a href=""> Vedi prodotto</a>
+         </div>
+    @endforeach
+</section>
+
+
+
 
 @endsection
